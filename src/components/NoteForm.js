769caf03bb78a6bceb99de069e0f25841903e0bd
Shailendra <shailendrashrivastav1292@gmail.com>
2024-06-23 23:21:00 +0530

    Updated the Submit Button

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -31,7 +31,7 @@ const NoteForm = ({ onSubmit }) => {
         type="submit"
         className="w-[120px] ml-[220px] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-md"
       >
-        Submit
+        Add Note
       </button>
     </form>
   );
